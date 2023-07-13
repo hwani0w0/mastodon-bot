64e74d340b1f45e83fb221e7576193b1bab5ccc0
hwani0w0 <hyehwan25@gmail.com>
2023-07-13 17:46:09 +0900

    upload

---
+++ CONTRIBUTING.md
@@ -0,0 +1,15 @@
+# Project Setup
+
+## dev environment
+
+```
+sudo apt install npm
+sudo npm install -g npx
+
+# maybe
+sudo npm install -g shadow-cljs
+
+# in project root to retrieve all dependencies
+npm install --ignore-scripts
+npx shadow-cljs compile test
+```
---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject dda/mastodon-bot "1.13.8-SNAPSHOT"
+  :description "Bot to publish twitter, tumblr or rss posts to an mastodon account."
+  :url "https://github.com/yogthos/mastodon-bot"
+  :author "Dmitri Sotnikov"
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :source-paths ["src/main/cljc"
+                 "src/main/clj"]
+  :resource-paths ["src/main/resources"]
+  :repositories [["releases" :clojars]
+                 ["snapshots" :clojars]]
+  :deploy-repositories [["snapshots" :clojars]
+                        ["releases" :clojars]]
+  :profiles {:test {:test-paths ["src/test/cljc"]
+                    :resource-paths ["src/test/resources"]
+                    :dependencies []}}
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag"]
+                  ["deploy"]
+                  ["change" "version" "leiningen.release/bump-version"]])
